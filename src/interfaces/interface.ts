2e7d5037965490767e076ad5e4049d0bb95e9d4a
harshsimform <harsh.v@simformsolutions.com>
2023-05-29 10:45:28 +0530

    removed react-multi-carousel library and added interface for banners

--- src/interfaces/interface.ts
+++ src/interfaces/interface.ts
@@ -1,6 +1,11 @@
 export interface NavItem {
-	label: string;
-	subLabel?: string;
-	children?: Array<NavItem>;
-	href?: string;
+  label: string;
+  subLabel?: string;
+  children?: Array<NavItem>;
+  href?: string;
+}
+
+export interface BannerItem {
+  id: number;
+  bannerImg: string;
 }
